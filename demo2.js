b2193e4c8b82827c6c764c31a1b7f30d0f915f20
Letheduy02 <duylt02122003@gmail.com>
2025-01-09 17:18:30 +0700

    commit lan 2

---
+++ demo2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to React Native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+});
